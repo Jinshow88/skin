cba455daec98fd86653a44e7c955d4640c36a0da
이진현 <ljun88@naver.com>
2025-12-06 13:12:27 +0900

    프로젝트 기초 만드는 중2

--- demon/src/main/java/com/example/demon/entity/ReviewEntity.java
+++ demon/src/main/java/com/example/demon/entity/ReviewEntity.java
@@ -1,5 +1,14 @@
 package com.example.demon.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "review")
 public class ReviewEntity {
-    
+
 }
